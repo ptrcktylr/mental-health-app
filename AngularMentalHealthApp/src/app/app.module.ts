8c46fce7fe945dcfe584ab40042101d11e0cb821
Len <lauren.wood@live.com>
2021-10-13 15:06:38 -0600

    Created components, api service, sentiment model

--- AngularMentalHealthApp/src/app/app.module.ts
+++ AngularMentalHealthApp/src/app/app.module.ts
@@ -12,6 +12,13 @@ import { ProfessionalViewProgressComponent } from './components/professionals/pr
 import { ProfessionalViewEntryComponent } from './components/professionals/professional-view-entry/professional-view-entry.component';
 import { ProfessionalAllPatientsComponent } from './components/professionals/professional-all-patients/professional-all-patients.component';
 import { ProfessionalAddPatientComponent } from './components/professionals/professional-add-patient/professional-add-patient.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PatientEntryComponent } from './patients/patient-entry/patient-entry.component';
+import { ViewPublicEntryComponent } from './public/view-public-entry/view-public-entry.component';
+import { PatientNavComponent } from './components/navbars/patient-nav/patient-nav.component';
+import { ProfessionalNavComponent } from './components/navbars/professional-nav/professional-nav.component';
+import { LoginComponent } from './components/portal/login/login.component';
+import { RegisterComponent } from './components/portal/register/register.component';
 
 @NgModule({
   declarations: [
@@ -24,11 +31,18 @@ import { ProfessionalAddPatientComponent } from './components/professionals/prof
     ProfessionalViewProgressComponent,
     ProfessionalViewEntryComponent,
     ProfessionalAllPatientsComponent,
-    ProfessionalAddPatientComponent
+    ProfessionalAddPatientComponent,
+    PatientEntryComponent,
+    ViewPublicEntryComponent,
+    PatientNavComponent,
+    ProfessionalNavComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularMentalHealthApp/src/app/components/navbars/professional-nav/professional-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-nav',
+  templateUrl: './professional-nav.component.html',
+  styleUrls: ['./professional-nav.component.css']
+})
+export class ProfessionalNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
