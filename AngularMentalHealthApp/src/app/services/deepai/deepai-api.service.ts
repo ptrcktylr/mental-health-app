cf41ad8b5536836b36b089494426c12cae3fb587
chang388 <beaksanpc388@gmail.com>
2021-10-14 14:31:29 -0700

    changed logic for sentimental analysis

--- AngularMentalHealthApp/src/app/services/deepai/deepai-api.service.ts
+++ AngularMentalHealthApp/src/app/services/deepai/deepai-api.service.ts
@@ -14,16 +14,8 @@ export class DeepaiApiService {
     const deepai = require('deepai'); // OR include deepai.min.js as a script tag in your HTML
 
     deepai.setApiKey('1bb26184-f8a5-4309-a691-179c2e3f0e83');
-    
-    (async function() {
-        var resp = await deepai.callStandardApi("sentiment-analysis", {
-                text: txtInput,
-        });
-        console.log(resp);
-        //return resp;
-    })();
 
-    //return null;
+    return await deepai.callStandardApi("sentiment-analysis", {text: txtInput});
 
   }
 
