7512dfb97d5d7619746d7ff788aad46e80375a5a
Len <lauren.wood@live.com>
2021-10-14 00:15:45 -0600

    Added Login HTML, navbar HTML, routing

--- AngularMentalHealthApp/src/app/app-routing.module.ts
+++ AngularMentalHealthApp/src/app/app-routing.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientHistoryComponent } from './components/patients/patient-history/patient-history.component';
+import { PatientNewComponent } from './components/patients/patient-new/patient-new.component';
+import { PatientProgressComponent } from './components/patients/patient-progress/patient-progress.component';
 import { LoginComponent } from './components/portal/login/login.component';
+import { RegisterComponent } from './components/portal/register/register.component';
+import { ProfessionalAllPatientsComponent } from './components/professionals/professional-all-patients/professional-all-patients.component';
+import { ProfessionalMyPatientsComponent } from './components/professionals/professional-my-patients/professional-my-patients.component';
+import { ViewPublicComponent } from './components/public/view-public/view-public.component';
 
 const routes: Routes = [
   {
@@ -9,7 +16,36 @@ const routes: Routes = [
   },
   {
     path: "",
-    component: LoginComponent
+    pathMatch: 'prefix',
+    redirectTo: "login"
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "patient-history",
+    component: PatientHistoryComponent
+  },
+  {
+    path: "patient-new",
+    component: PatientNewComponent
+  },
+  {
+    path: "patient-progress",
+    component: PatientProgressComponent
+  },
+  {
+    path: "my-patients",
+    component: ProfessionalMyPatientsComponent
+  },
+  {
+    path: "all-patients",
+    component: ProfessionalAllPatientsComponent
+  },
+  {
+    path: "view-public",
+    component: ViewPublicComponent
   },
 ];
 
