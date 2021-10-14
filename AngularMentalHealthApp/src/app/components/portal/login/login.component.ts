208dd344f9abb83f2c5fd78ddab3234ce5a00c7c
Len <lauren.wood@live.com>
2021-10-14 00:15:45 -0600

    Added Login HTML, navbar HTML, routing

--- AngularMentalHealthApp/src/app/components/portal/login/login.component.ts
+++ AngularMentalHealthApp/src/app/components/portal/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
@@ -9,9 +10,41 @@ export class LoginComponent implements OnInit {
 
   public username:String = "";
   public password:String = "";
-  constructor() { }
+  public error:String = "";
 
-  login():void{
+  constructor(private route:Router) { }
+
+  resetError(): void{
+    this.error = "";
+  }
+
+  login(): void{
+    // code to make it functional until I have connection to the db
+    if(this.username == "username" && this.password == "password"){
+      this.route.navigate(['/patient-history']);
+    }
+    else if(this.username == "professional" && this.password == "password"){
+      this.route.navigate(['/my-patients']);
+    }
+    else{
+      this.error = "Username or Password not recognized.";
+    }
+
+    //actual code will look more like this:
+    /* 
+    user = getUserByKeys(username,password)
+    if(user != null){
+      if(user.role == patient){
+        this.route.navigate(['/patient-history']);
+      }
+      else if(user.role == professional){
+        this.route.navigate(['/professional-all-patients']);
+      }
+    }
+    else{
+      error = "Username or password not recognized."
+    }
+    */
 
   }
 
