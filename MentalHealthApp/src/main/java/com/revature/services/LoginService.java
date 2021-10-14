d58cf079f02e1626358c6f0f0f65492e0f04b293
ptrcktylr <ptrcktylr@icloud.com>
2021-10-14 18:16:13 -0400

    implemented login service

--- MentalHealthApp/src/main/java/com/revature/services/LoginService.java
+++ MentalHealthApp/src/main/java/com/revature/services/LoginService.java
@@ -1,5 +1,13 @@
 package com.revature.services;
 
+import com.revature.daos.UserDao;
+import com.revature.models.User;
+
 public class LoginService {
 
+	public User userLogin(String username, String password) {
+		UserDao UDao = new UserDao();
+		return UDao.validLogin(username, password);
+	}
+	
 }
