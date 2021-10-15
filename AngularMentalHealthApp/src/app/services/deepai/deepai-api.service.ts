222f0b01f97f1cd46337d070dacb1418f79e0984
chang388 <beaksanpc388@gmail.com>
2021-10-15 12:00:05 -0700

    changed deepai service again

--- AngularMentalHealthApp/src/app/services/deepai/deepai-api.service.ts
+++ AngularMentalHealthApp/src/app/services/deepai/deepai-api.service.ts
@@ -15,15 +15,7 @@ export class DeepaiApiService {
 
     deepai.setApiKey('1bb26184-f8a5-4309-a691-179c2e3f0e83');
     
-    (async function() {
-        var resp = await deepai.callStandardApi("sentiment-analysis", {
-                text: txtInput,
-        });
-        console.log(resp);
-        //return resp;
-    })();
-
-    //return null;
+    return await deepai.callStandardApi("sentiment-analysis", {text: txtInput});
 
   }
 
