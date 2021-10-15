3f14579512d053bf313a585fb64395b26d73df31
wood-len <54560238+wood-len@users.noreply.github.com>
2021-10-14 22:29:20 -0600

    Patient Progress finished

--- AngularMentalHealthApp/src/app/components/patients/patient-progress/patient-progress.component.ts
+++ AngularMentalHealthApp/src/app/components/patients/patient-progress/patient-progress.component.ts
@@ -6,38 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-progress.component.css']
 })
 export class PatientProgressComponent implements OnInit {
-  
   dataSource: Object;
   constructor() {
     //STEP 2 - Chart Data
     const chartData = [
       {
-        label: "10/14/2021",
-        value: 1
+        label: "Mon",
+        value: 75
       },
       {
-        label: "10/15/2021",
-        value: -1
+        label: "Tues",
+        value: 20
       },
       {
-        label: "10/16/2021",
-        value: 0
+        label: "Wed",
+        value: 47
       },
       {
-        label: "10/17/2021",
-        value: 0
+        label: "Thurs",
+        value: 59
       },
       {
-        label: "10/18/2021",
-        value: -1
+        label: "Fri",
+        value: 15
       },
       {
-        label: "10/19/2021",
-        value: 1
+        label: "Sat",
+        value: 69
       },
       {
-        label: "10/20/2021",
-        value: -1
+        label: "Sun",
+        value: 40
       },
     ];
     // STEP 3 - Chart Configuration
@@ -48,9 +47,11 @@ export class PatientProgressComponent implements OnInit {
         //Set the chart subcaption
         // subCaption: "In MMbbl = One Million barrels",
         //Set the x-axis name
-        xAxisName: "Date",
+        xAxisName: "Day",
         //Set the y-axis name
-        yAxisName: "<--Negative_____Positive-->",
+        yAxisName: "<-Negative        Positive->",
+        yAxisMaxValue: 100.01,
+        yAxisMinValue: 0,
         showYAxisValues: '0',
         //Set the theme for your chart
         theme: "fusion"
