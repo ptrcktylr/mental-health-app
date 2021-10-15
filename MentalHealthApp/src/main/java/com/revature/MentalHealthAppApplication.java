a1f6565684d6f7e9aa56105655797a34c42172f2
ptrcktylr <ptrcktylr@icloud.com>
2021-10-15 09:40:47 -0400

    removed dao testing and uncommented spring application runner

--- MentalHealthApp/src/main/java/com/revature/MentalHealthAppApplication.java
+++ MentalHealthApp/src/main/java/com/revature/MentalHealthAppApplication.java
@@ -18,27 +18,7 @@ import com.revature.services.UserService;
 public class MentalHealthAppApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(MentalHealthAppApplication.class, args);
-		
-//		UserDao UDao = new UserDao();
-//		EntryDao EDao = new EntryDao();
-//		ReplyDao RDao = new ReplyDao();
-		
-		PatientService pts = new PatientService();
-		ProfessionalService prs = new ProfessionalService();
-		LoginService ls = new LoginService();
-		UserService us = new UserService();
-		
-		User u1 = new User("user1", "p@ssw0rd", false, "Joe", "Smith", "js@yahoo.com");
-		User u2 = new User("user2", "p@ssw0rd", true, "Sam", "Parker", "sp@yahoo.com");
-		User u3 = new User("user3", "password", false, "Bob", "Bobby", "bb@yahoo.com");
-		User u4 = new User("user4", "abcdefg", true, "John", "Jacob", "jj@yahoo.com");
-				
-		us.addUser(u1);
-		us.addUser(u2);
-		us.addUser(u3);
-		us.addUser(u4);
-		
+		SpringApplication.run(MentalHealthAppApplication.class, args);
 	}
 
 }
