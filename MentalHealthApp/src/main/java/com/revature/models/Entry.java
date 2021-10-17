bb32d6dffac94967d0b6ae21c80fc36868060ea9
ptrcktylr <ptrcktylr@icloud.com>
2021-10-17 08:56:41 -0400

    removed json ignore from entry patient (author) info

--- MentalHealthApp/src/main/java/com/revature/models/Entry.java
+++ MentalHealthApp/src/main/java/com/revature/models/Entry.java
@@ -17,7 +17,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="entries")
