a398063e481b79381a646d5f1618450d69dfa1f7
ptrcktylr <ptrcktylr@icloud.com>
2021-10-20 17:38:46 -0400

    added username to patient navbar

--- AngularMentalHealthApp/src/app/components/navbars/patient-nav/patient-nav.component.ts
+++ AngularMentalHealthApp/src/app/components/navbars/patient-nav/patient-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { PatientService } from 'src/app/services/patients/patient.service';
 import { PortalService } from 'src/app/services/portal/portal.service';
 
 @Component({
@@ -13,9 +14,19 @@ export class PatientNavComponent implements OnInit {
   username:String = "get username"
   showMenu:Boolean = true;
 
-  constructor(private route:Router,private cookie:CookieService,private portalS:PortalService) { }
+  constructor(private route:Router,private cookie:CookieService,private portalS:PortalService, private patientS:PatientService) { }
 
   ngOnInit(): void {
+
+    this.patientS.getMyInfo().subscribe(
+      (myInfo:any)=>{
+        this.username = myInfo.username;
+      },
+      ()=>{
+        console.log("No user information");
+      }
+    );
+
   }
 
   logout():void{
